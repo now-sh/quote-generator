455e09e6df1c08b141d437ed7aa870daa35235fe
Jason <jason@casjay.net>
2021-11-10 15:08:54 -0500

    🗃 Modified: .gitignore README.md package-lock.json package.json public/index.html src/App.vue src/components/Quote.vue  🗃

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-	<div class="app">
-		<Header title="The Anime Quoter" />
-		<Quote :quote="quote" />
-		<div class="button-container">
-			<button @click="fetchQuote">GET A NEW QUOTE</button>
-		</div>
-		<QuoteList :quotes="quotes" />
-	</div>
+  <div class="app">
+    <div class="vh-100">
+      <Header title="The Anime Quoter" />
+      <Quote :quote="quote" />
+      <div class="button-container">
+        <button @click="fetchQuote">GET A NEW QUOTE</button>
+      </div>
+    </div>
+    <QuoteList :quotes="quotes" />
+  </div>
 </template>
 
 <script>
@@ -15,83 +17,85 @@ import Quote from './components/Quote.vue';
 import QuoteList from './components/QuoteList.vue';
 
 export default {
-	name: 'App',
-	components: {
-		Header,
-		Quote,
-		QuoteList
-	},
-	data() {
-		return {
-			quote: {
-				content: '',
-				character: '',
-				anime: ''
-			},
-			quotes: []
-		}
-	},
-	created () {
-		this.fetchQuote();
-	},
-	methods: {
-		async fetchQuote () {
-			const data = await fetch('https://animechan.vercel.app/api/random').then(res => res.json());
+  name: 'App',
+  components: {
+    Header,
+    Quote,
+    QuoteList,
+  },
+  data() {
+    return {
+      quote: {
+        content: '',
+        character: '',
+        anime: '',
+      },
+      quotes: [],
+    };
+  },
+  created() {
+    this.fetchQuote();
+  },
+  methods: {
+    async fetchQuote() {
+      const data = await fetch(
+        'https://animechan.vercel.app/api/random'
+      ).then((res) => res.json());
 
-			if (this.quote.content) {
-				this.quotes = [...this.quotes, this.quote];
-			}
+      if (this.quote.content) {
+        this.quotes = [...this.quotes, this.quote];
+      }
 
-			this.quote = {
-				anime: data.anime,
-				character: data.character,
-				content: data.quote
-			}
-		}
-	}
-}
+      this.quote = {
+        anime: data.anime,
+        character: data.character,
+        content: data.quote,
+      };
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 :root {
-	--primary: #D81E5B;
-	--secondary: #8A4FFF;
-	--tertiary: #32CBFF;
-	--dark: #131A26;
-	--light: #EEE;
-	--grey: #848484;
+  --primary: #d81e5b;
+  --secondary: #8a4fff;
+  --tertiary: #32cbff;
+  --dark: #131a26;
+  --light: #eee;
+  --grey: #848484;
 }
 
 * {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: 'Fira Sans', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
 }
 
 .button-container {
-	display: flex;
-	justify-content: center;
-	padding: 0px 32px;
-	margin: 64px auto;
+  display: flex;
+  justify-content: center;
+  padding: 0px 32px;
+  margin: 64px auto;
 
-	button {
-		border: none;
-		outline: none;
-		background-color: var(--primary);
-		padding: 16px 32px;
-		border-radius: 99px;
+  button {
+    border: none;
+    outline: none;
+    background-color: var(--primary);
+    padding: 16px 32px;
+    border-radius: 99px;
 
-		color: var(--light);
-		font-size: 1.5em;
-		font-weight: 700;
-		text-transform: uppercase;
-		cursor: pointer;
-		transition: 0.4s;
+    color: var(--light);
+    font-size: 1.5em;
+    font-weight: 700;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: 0.4s;
 
-		&:hover {
-			background-color: var(--secondary);
-		}
-	}
+    &:hover {
+      background-color: var(--secondary);
+    }
+  }
 }
 </style>
